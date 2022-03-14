2b41fc27fa7e375637ddeb740d8c2ad422f789f0
Gareth Leenderts <garethleenderts@gmail.com>
2022-03-14 12:47:49 +0200

    Setup initial socket.io connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const http = require('http');
+const socketio = require('socket.io');
 
 
 require('dotenv').config();
 
 const app = express();
-
 const server = http.createServer(app);
+const io = socketio(server);
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New web socket connection...');
+});
+
+
 
 const PORT = 3000 || process.env.PORT;
 
