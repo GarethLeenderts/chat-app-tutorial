61bf805ea425866ef746d007622e247c47eebb5a
Gareth Leenderts <garethleenderts@gmail.com>
2022-03-14 12:43:41 +0200

    Bring http module in

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const http = require('http');
+
 
 require('dotenv').config();
 
 const app = express();
 
+const server = http.createServer(app);
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
